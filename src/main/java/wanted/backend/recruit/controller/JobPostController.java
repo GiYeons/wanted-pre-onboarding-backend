eec9b6e271a9d31aa1b6226582fbdba34f5e1680
GiYeons <65033360+GiYeons@users.noreply.github.com>
2024-08-09 18:07:03 +0900

    Chore: MySQL 연동

---
+++ src/main/java/wanted/backend/recruit/controller/JobPostController.java
@@ -0,0 +1,9 @@
+package wanted.backend.recruit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobPostController {
+//    @GetMapping("/")
+}
